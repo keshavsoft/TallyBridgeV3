fa8d2d3d1def6199a78d3558a7d5b80009d09835
keshavsoft <nknnkumar@yahoo.com>
2026-04-14 11:41:04 +0530

    masters from tally perfect

--- Api/V3/StockItems/routes.js
+++ Api/V3/StockItems/routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { router as routerFromGetFromTally } from "./GetFromTally/endpoints.js";
+import { postFilterDataFromBodyFunc } from "./Transform/controller.js";
 
 const router = express.Router();
 router.use('/GetFromTally', routerFromGetFromTally);
+router.use('/Transform', postFilterDataFromBodyFunc);
 
 export { router };
